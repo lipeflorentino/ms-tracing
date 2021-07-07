0533b18c9559c65808ce93209d59f1af54302cce
lipeflorentino <filipe@bgcbrasil.com.br>
2021-07-07 20:34:45 -0300

    master updates

---
+++ init-project.sh
@@ -0,0 +1,4 @@
+git init
+mkdir config sls src tests
+touch .babelrc webpack.config.js .gitignore .npmignore
+npm install babel-loader babel-preset-env @babel/core webpack-node-externals webpack serverless-webpack aws-sdk
