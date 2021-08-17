870409daca902a626406d381e45fcb5c907fb24e
lipeflorentino <filipe@bgcbrasil.com.br>
2021-08-17 13:54:39 -0300

    dev updates

--- models/transaction.js
+++ models/transaction.js
@@ -2,13 +2,10 @@ import * as dynamoose from 'dynamoose';
 
 import moment from 'moment';
 
-import { v5 as uuid } from 'uuid';
-
 const TransactionSchema = new dynamoose.Schema({
   transactionId: {
     type: String,
     hashKey: true,
-    default: uuid('https://www.w3.org/', 'c3e23e31-83ea-4a96-9ead-49029c96abdd'),
   },
   createdAt: {
     type: String,
